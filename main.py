445cad62645d7a16752764151bfc4a34d1deac6d
cameradias <cameradias@gmail.com>
2018-09-16 22:47:14 -0700

    while loop cleaned up

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import firstprogram
 
-b = True
-n = 0
-while (b == True):
-    n = input("Enter -1 to quit")
-    n = int(n)
-    if (n == -1):
-        b = False
-        break
-
+n = int(input("Enter size in bytes (-1 to quit): "))
+while (n != -1):
     print(firstprogram.approximate_size(n))
     # print(approximate_size(n,False))
-    # print (n)
+    n = int(input("Enter size in bytes (-1 to quit): "))
 
 print("Quiting main")
