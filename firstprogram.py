8a5d3c0106c896bcc7437dfa791782174351dc53
cameradias <cameradias@gmail.com>
2018-09-16 21:39:02 -0700

    formatted programs

--- firstprogram.py
+++ firstprogram.py
@@ -1,5 +1,7 @@
 SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+            1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+
+
 def approximate_size(size, a_kilobyte_is_1024_bytes=True):
     '''Convert a file size to human-readable form.
     Keyword arguments:
@@ -16,6 +18,8 @@ def approximate_size(size, a_kilobyte_is_1024_bytes=True):
         if size < multiple:
             return '{0:.1f} {1}'.format(size, suffix)
     raise ValueError('number too large')
+
+
 if __name__ == '__main__':
     print(approximate_size(1000000000000, False))
     print(approximate_size(1000000000000))
